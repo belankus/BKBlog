52ddf5813c330de0823b56cbdf00e213afc694ba
belankus <belankus@gmail.com>
2024-01-13 03:06:01 +0700

    Fix navbar background onload

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -142,6 +142,50 @@
                             </a>
                         </div>
                     </article>
+                    <article
+                        class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                        <div class="flex justify-between items-center mb-5 text-gray-500">
+                            <span
+                                class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                                        clip-rule="evenodd"></path>
+                                    <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+                                </svg>
+                                Article
+                            </span>
+                            <span class="text-sm">14 days ago</span>
+                        </div>
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
+                                href="#">Our
+                                first project with React</a></h2>
+                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to
+                            bootstrap lots of websites and are becoming the basis for a variety of tools that even
+                            influence both web designers and developers influence both web designers and developers.
+                        </p>
+                        <div class="flex justify-between items-center">
+                            <div class="flex items-center space-x-4">
+                                <img class="w-7 h-7 rounded-full"
+                                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                                    alt="Bonnie Green avatar" />
+                                <span class="font-medium dark:text-white">
+                                    Bonnie Green
+                                </span>
+                            </div>
+                            <a href="#"
+                                class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                More
+                                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </article>
                 </div>
                 <aside class="sticky top-[6rem] ml-8 w-full h-full">
                     <span class="sr-only">Blog Sidebar</span>
--- resources/views/layouts/templates/main.blade.php
+++ resources/views/layouts/templates/main.blade.php
@@ -3,82 +3,7 @@
 <body>
     <div class="bg-white">
         {{-- Navbar --}}
-        <header id="navbar" x-data="{ openSidebar: false }" @scroll.window="stickyNavbar"
-            class="fixed inset-x-0 top-0 z-50 transition duration-500 ease-in-out">
-            <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-                <div class="flex lg:flex-1">
-                    <a href="#" class="-m-1.5 p-1.5">
-                        <span class="sr-only">BKA Blog</span>
-                        <h1 class="text-2xl font-normal"><span class="font-bold">BKA</span> Blog</h1>
-                    </a>
-                </div>
-                <div class="flex lg:hidden">
-                    <button @click="openSidebar = true;" type="button"
-                        class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                            aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d=" M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="hidden lg:flex lg:gap-x-12">
-                    <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
-                    <a href="/blog" class="text-sm font-semibold leading-6 text-gray-900">Blog</a>
-                    <a href="/about" class="text-sm font-semibold leading-6 text-gray-900">About</a>
-                    <a href="/contact" class="text-sm font-semibold leading-6 text-gray-900">Contact</a>
-                </div>
-                <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
-                            aria-hidden="true">&rarr;</span></a>
-                </div>
-            </nav>
-            <!-- Mobile menu, show/hide based on menu open state. -->
-            <div x-show="openSidebar" x-transition:enter="transition ease-out duration-500"
-                x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-                x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100"
-                x-transition:leave-end="opacity-0" class="lg:hidden" role="dialog" aria-modal="true">
-                <!-- Background backdrop, show/hide based on slide-over state. -->
-                <div class="fixed inset-0 z-50"></div>
-                <div
-                    class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div class="flex items-center justify-between">
-                        <a href="#" class="-m-1.5 p-1.5">
-                            <span class="sr-only">BKA Blog</span>
-                            <h1 class="text-2xl font-normal"><span class="font-bold">BKA</span> Blog</h1>
-                        </a>
-                        <button @click="openSidebar = false;" type="button"
-                            class="-m-2.5 rounded-md p-2.5 text-gray-700">
-                            <span class="sr-only">Close menu</span>
-                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="mt-6 flow-root">
-                        <div class="-my-6 divide-y divide-gray-500/10">
-                            <div class="space-y-2 py-6">
-                                <a href="/"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
-                                <a href="/blog"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Blog</a>
-                                <a href="/about"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>
-                                <a href="/contact"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact</a>
-                            </div>
-                            <div class="py-6">
-                                <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
-                                    in</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
+        @include('layouts.templates.navbar')
 
         <div class="relative isolate px-6 pt-14 lg:px-8">
             {{-- Blob Warna Warni --}}
@@ -157,18 +82,31 @@
         </footer>
     </div>
     <script>
-        let navbar = document.querySelector("#navbar");
+        const nav = document.getElementById('navbar');
+        const header = document.getElementById('pseudo-navbar');
+        const navHeight = nav.getBoundingClientRect().height;
 
-        function stickyNavbar(e) {
-            if (scrollY > 10) {
-                navbar.classList.add('bg-white', 'shadow');
+        function updateNavColor(entries) {
+            const [entry] = entries;
+            if (!entry.isIntersecting) {
+                nav.classList.add('bg-white', 'shadow');
+                // nav.classList.remove("transparan");
+
             } else {
-                navbar.classList.remove('bg-white', 'shadow');
-
+                // nav.classList.add("transparan");
+                nav.classList.remove('bg-white', 'shadow');
             }
-        }
-    </script>
 
+        }
+
+        const headerObserver = new IntersectionObserver(updateNavColor, {
+            root: null,
+            threshold: 0,
+            rootMargin: `-${navHeight}px`
+        });
+
+        headerObserver.observe(header)
+    </script>
 </body>
 
 </html>
