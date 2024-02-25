8d2e4c507742a648ced83eb37d5ea802c1163818
belankus <belankus@gmail.com>
2024-02-25 23:41:49 +0700

    dev: add popup image, add link color

--- resources/views/layouts/blog/content.blade.php
+++ resources/views/layouts/blog/content.blade.php
@@ -2,24 +2,34 @@
     $content = json_decode($singlePost->content, true);
 @endphp
 
+
 @foreach ($content['blocks'] as $block)
     @if ($block['type'] == 'header')
-        <h{{ $block['data']['level'] }} class="font-bold">
+        <h{{ $block['data']['level'] }} class="text-{{ 6 - $block['data']['level'] }}xl mb-2.5 font-bold">
             {!! $block['data']['text'] !!}</h{{ $block['data']['level'] }}>
         @elseif($block['type'] == 'paragraph')
-            <p class="hyphens-auto whitespace-pre-wrap break-words text-justify text-gray-500">{!! $block['data']['text'] !!}
+            <p
+                class="hyphens-auto break-words text-justify text-lg leading-relaxed text-gray-500 [&>a]:text-blue-500 [&>a]:underline [&>a]:transition hover:[&>a]:text-blue-600 hover:[&>a]:no-underline">
+                {!! $block['data']['text'] !!}
             </p>
         @elseif($block['type'] == 'list')
-            {!! $block['data']['style'] == 'ordered' ? '<ol>' : '<ul>' !!}
+            {!! $block['data']['style'] == 'ordered'
+                ? '<ol class="list-decimal list-inside indent-10 text-lg leading-relaxed">'
+                : '<ul class="list-disc list-inside indent-10 text-lg leading-relaxed">' !!}
             @foreach ($block['data']['items'] as $item)
-                <li class="">{!! $item !!}</li>
+                <li
+                    class="text-gray-500 [&>a]:text-blue-500 [&>a]:underline [&>a]:transition hover:[&>a]:text-blue-600 hover:[&>a]:no-underline">
+                    {!! $item !!}</li>
             @endforeach
             {!! $block['data']['style'] == 'ordered' ? '</ol>' : '</ul>' !!}
         @elseif($block['type'] == 'image')
-            <div class="flex w-full justify-center">
+            <div class="post-gallery flex w-full justify-center">
                 <div
                     class="{{ $block['data']['withBorder'] == 'true' ? 'border' : '' }} {{ $block['data']['withBackground'] == 'true' ? 'bg-gray-100' : '' }} {{ $block['data']['stretched'] == 'true' ? 'w-full' : 'max-w-lg' }} my-8 flex flex-col overflow-hidden rounded-lg border-gray-400 shadow-lg">
-                    <img src="{{ $block['data']['file']['url'] }}" alt="{!! $block['data']['caption'] !!}">
+                    <a class="lightbox" href="{{ $block['data']['file']['url'] }}"
+                        data-caption="{{ $block['data']['caption'] }}">
+                        <img src="{{ $block['data']['file']['url'] }}" alt="{!! $block['data']['caption'] !!}">
+                    </a>
                     <p
                         class="{{ $block['data']['withBorder'] == 'true' ? 'border-t' : '' }} mt-0 border-gray-400 py-2 text-center text-sm text-gray-400">
                         {!! $block['data']['caption'] !!}</p>
--- resources/views/layouts/blog/head.blade.php
+++ resources/views/layouts/blog/head.blade.php
@@ -10,6 +10,9 @@
     <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css"
+        integrity="sha512-NVt7pmp5f+3eWRPO1h4A1gCf4opn4r5z2wS1mi7AaVcTzE9wDJ6RzMqSygjDzYHLp+mAJ2/qzXXDHar6IQwddQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @if (env('APP_ENV') == 'production')
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-E5DM43VX3L"></script>
