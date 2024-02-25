12d422019e72e49d8821e187c9b71ab0d7a88768
belankus <belankus@gmail.com>
2024-02-25 22:38:34 +0700

    dev: add image behaviour on post by editor configuration

--- resources/views/layouts/blog/content.blade.php
+++ resources/views/layouts/blog/content.blade.php
@@ -7,7 +7,8 @@
         <h{{ $block['data']['level'] }} class="font-bold">
             {!! $block['data']['text'] !!}</h{{ $block['data']['level'] }}>
         @elseif($block['type'] == 'paragraph')
-            <p class="break-all text-justify">{!! $block['data']['text'] !!}</p>
+            <p class="hyphens-auto whitespace-pre-wrap break-words text-justify text-gray-500">{!! $block['data']['text'] !!}
+            </p>
         @elseif($block['type'] == 'list')
             {!! $block['data']['style'] == 'ordered' ? '<ol>' : '<ul>' !!}
             @foreach ($block['data']['items'] as $item)
@@ -15,8 +16,14 @@
             @endforeach
             {!! $block['data']['style'] == 'ordered' ? '</ol>' : '</ul>' !!}
         @elseif($block['type'] == 'image')
-            <div>
-                <img src="{{ $block['data']['file']['url'] }}" alt="{{ $block['data']['caption'] }}">
+            <div class="flex w-full justify-center">
+                <div
+                    class="{{ $block['data']['withBorder'] == 'true' ? 'border' : '' }} {{ $block['data']['withBackground'] == 'true' ? 'bg-gray-100' : '' }} {{ $block['data']['stretched'] == 'true' ? 'w-full' : 'max-w-lg' }} my-8 flex flex-col overflow-hidden rounded-lg border-gray-400 shadow-lg">
+                    <img src="{{ $block['data']['file']['url'] }}" alt="{!! $block['data']['caption'] !!}">
+                    <p
+                        class="{{ $block['data']['withBorder'] == 'true' ? 'border-t' : '' }} mt-0 border-gray-400 py-2 text-center text-sm text-gray-400">
+                        {!! $block['data']['caption'] !!}</p>
+                </div>
             </div>
     @endif
 @endforeach
