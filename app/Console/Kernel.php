e45b57d5eee606b35b693ae0f16bb4c759e58008
belankus <belankus@gmail.com>
2024-02-26 00:46:47 +0700

    patch: schedule temp image daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () use ($postController) {
             $postController->cleanupTemporaryImages();
-        })->everyMinute();
+        })->daily();
     }
 
     /**
